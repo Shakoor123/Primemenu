d264f097082cc77b152caf5065b73235ebf51036
shakoor123 <shakoorchachu2001@gmail.com>
2024-02-22 13:11:41 +0530

    created new home page for Hamilton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import Home from "./pages/Home";
+import Home from "./components/hamilton/home/Home.jsx";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/host/3f9bd1a2-33f7-4421-824e-9aaad9a8196d"
+              element={<Home />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <div className="App-non-mobile">
+        <span className="h-title">Availbale on mobile only</span>
+      </div>
+    </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Restaurant from "../components/restaurant/Restaurant";
 import RestaurantUserInput from "../components/restaurantUserInput/RestaurantUserInput";
 import RestaurantBrandInput from "../components/restaurantBrandInput/RestaurantBrandInput";
+import Header from "../components/hamilton/header/Header";
 
 export default function Home() {
   return (
     <div>
-      <Restaurant />
+      <Header />
+      {/* <Restaurant /> */}
       {/* <RestaurantUserInput /> */}
       {/* <RestaurantBrandInput /> */}
     </div>
